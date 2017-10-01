8ac7f3648bde349bdb558303934fc4485e17fe8d
Demetrio <dem3perez@gmail.com>
2017-10-01 00:33:37 -0400

    edit Tests

--- SpaceInvaders/SpaceInvaders/main.cpp
+++ SpaceInvaders/SpaceInvaders/main.cpp
@@ -5,6 +5,7 @@
 #include "SDLManagerComponent.hpp"
 #include "SpriteComponent.hpp"
 #include "MessageComponent.hpp"
+#include "HeroBehaviorComponent.hpp"
 #include "InputManager.hpp"
 
 using namespace std;
@@ -32,30 +33,14 @@ int test()
     
     GameObject * test = new GameObject();
     GameObject * test2 = new GameObject();
+    
     test->components.push_back(new SpriteComponent("img/bg.png",700,700));
     test2->components.push_back(new SpriteComponent("img/arwing.png",55,39));
-    
+    test2->components.push_back(new HeroBehaviorComponent());
     
     while (true)
     {
         InputManager::update();
-        if(InputManager::controller.back)
-        {
-            test2->y += 1;
-        }
-        if(InputManager::controller.forward)
-        {
-            test2->y -= 1;
-        }
-        if(InputManager::controller.left)
-        {
-            test2->x -= 1;
-        }
-        if(InputManager::controller.right)
-        {
-            test2->x += 1;
-        }
-        
         SDLManagerComponent::clear();
         test->update();
         test2->update();
