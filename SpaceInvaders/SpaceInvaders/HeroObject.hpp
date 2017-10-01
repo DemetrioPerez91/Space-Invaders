c68558b26f86815a6c5403b81e518996536b7bfb
Demetrio <dem3perez@gmail.com>
2017-10-01 00:33:24 -0400

    Add HeroObject

---
+++ SpaceInvaders/SpaceInvaders/HeroObject.hpp
@@ -0,0 +1,20 @@
+//
+//  HeroObject.hpp
+//  SpaceInvaders
+//
+//  Created by User on 10/1/17.
+//  Copyright © 2017 DemetrioPerez. All rights reserved.
+//
+
+#ifndef HeroObject_hpp
+#define HeroObject_hpp
+
+#include <stdio.h>
+#include "GameObject.hpp"
+class HeroObject: public GameObject
+{
+    
+};
+
+
+#endif /* HeroObject_hpp */
