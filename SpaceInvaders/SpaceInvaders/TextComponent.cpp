693bf8be219570e7ac7845b08c8ec0b1715cf5ab
Demetrio <dem3perez@gmail.com>
2017-10-02 22:17:45 -0400

    Add TextComponent

---
+++ SpaceInvaders/SpaceInvaders/TextComponent.cpp
@@ -0,0 +1,9 @@
+//
+//  TextComponent.cpp
+//  SpaceInvaders
+//
+//  Created by User on 10/2/17.
+//  Copyright © 2017 DemetrioPerez. All rights reserved.
+//
+
+#include "TextComponent.hpp"
---
+++ SpaceInvaders/SpaceInvaders/TextComponent.hpp
@@ -0,0 +1,16 @@
+//
+//  TextComponent.hpp
+//  SpaceInvaders
+//
+//  Created by User on 10/2/17.
+//  Copyright © 2017 DemetrioPerez. All rights reserved.
+//
+
+#ifndef TextComponent_hpp
+#define TextComponent_hpp
+
+#include <stdio.h>
+#include "GameComponent.h"
+
+
+#endif /* TextComponent_hpp */
