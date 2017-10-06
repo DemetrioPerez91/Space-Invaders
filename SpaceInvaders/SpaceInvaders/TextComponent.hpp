6d696961973a5565ea274645fc23f9c9eb4f50f4
Demetrio <dem3perez@gmail.com>
2017-10-06 08:37:24 -0400

    WIP text component

--- SpaceInvaders/SpaceInvaders/TextComponent.hpp
+++ SpaceInvaders/SpaceInvaders/TextComponent.hpp
@@ -11,6 +11,32 @@
 
 #include <stdio.h>
 #include "GameComponent.h"
+#include "SDLManagerComponent.hpp"
+
+class TextComponent: public GameComponent
+{
+public:
+    TextComponent(std::string text,std::string fontURL, GameObject * owner)
+    {
+        this->text = text;
+        this->fontURL = fontURL;
+        this->owner = owner;
+    }
+    
+    
+    TTF_Font * font = NULL;
+    std::string text = "";
+    std::string fontURL = "";
+    
+    SDL_Texture * textTexture = NULL;
+    
+    bool isTextUpdated = false;
+    void start();
+    void update();
+    
+    
+};
+
 
 
 #endif /* TextComponent_hpp */
